085578e52945b6b3062513e522660335a9bc7c09
Basil Eldho <52128422+basileldho2003@users.noreply.github.com>
2025-09-24 23:36:30 +0530

    Attempt to add brew

--- build_files/build.sh
+++ build_files/build.sh
@@ -14,13 +14,23 @@ dnf5 install -y hello
 
 # Use a COPR Example:
 #
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
+dnf5 -y copr enable ublue-os/staging
+
+dnf5 -y copr enable ublue-os/packages
+
+dnf5 -y install ublue-brew uupd
+
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
 
 #### Example for enabling a System Unit File
 
+systemctl disable rpm-ostreed-automatic.timer
+systemctl disable flatpak-system-update.timer
+systemctl --global disable flatpak-user-update.timer
+systemctl disable brew-update.timer
+systemctl disable brew-upgrade.timer
+systemctl enable uupd.timer
 systemctl enable podman.socket
 
 dnf5 clean all
