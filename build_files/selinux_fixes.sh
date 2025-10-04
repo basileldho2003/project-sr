cf96d8001c197a0a929b21ba5557d13c642118cc
Basil Eldho <52128422+basileldho2003@users.noreply.github.com>
2025-10-04 18:05:35 +0530

    Fix both SELinux issues.

--- build_files/selinux_fixes.sh
+++ build_files/selinux_fixes.sh
@@ -11,6 +11,7 @@ semanage fcontext -a -t xdm_home_t '/var/home/[^/]+/.dmrc'
 
 # --- tuned config directory ---
 # tuned-ppd needs to write/search inside /etc/tuned
+semanage fcontext -a -t tuned_rw_etc_t '/etc/tuned'
 semanage fcontext -a -t tuned_rw_etc_t '/etc/tuned(/.*)?'
 
 # --- Plymouth boot log ---
@@ -18,7 +19,6 @@ semanage fcontext -a -t tuned_rw_etc_t '/etc/tuned(/.*)?'
 semanage fcontext -a -t plymouthd_var_log_t '/var/log/boot.log'
 
 # Apply changes immediately
-restorecon -Rv /home || true
 restorecon -Rv /var/home || true
 restorecon -Rv /etc/tuned || true
 restorecon -v /var/log/boot.log || true
